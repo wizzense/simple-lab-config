a496fc213c2db8891637dc022e55198af6ee0ac6
wizzense <37890504+wizzense@users.noreply.github.com>
2025-03-05 16:02:30 -0800

    ghauth

--- kicker.ps1
+++ kicker.ps1
@@ -146,7 +146,7 @@ catch {
         Write-Host "GitHub CLI is now authenticated."
     }
     catch {
-        Write-Error "ERROR: GitHub authentication failed. Please log in manually using '$($ghExePath) auth login'. Then re-run the commands!"
+        Write-Error "ERROR: GitHub authentication failed. Please run '$($ghExePath) auth login'. Then re-run the commands!"
         exit 
     }
 }
