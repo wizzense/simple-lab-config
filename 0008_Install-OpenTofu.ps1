7cabf0fa508ae5633b2d31f23e44afb7130d5f86
wizzense <37890504+wizzense@users.noreply.github.com>
2025-03-07 08:01:47 -0800

    add validation tools setup

--- 0008_Install-OpenTofu.ps1
+++ 0008_Install-OpenTofu.ps1
@@ -5,5 +5,6 @@ param(
     )
 
 if ($Config.InstallOpenTofu -eq $true) {
-    & .\OpenTofuInstaller.ps1 -installMethod standalone -skipVerify
+    $Cosign = Join-Path $Config.CosignPath "cosign-windows-amd64.exe"
+    & .\OpenTofuInstaller.ps1 -installMethod standalone -cosignPath $Cosign
 }
