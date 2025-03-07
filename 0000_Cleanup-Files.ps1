931949175757e2bea380f43d611c71d81fcaba1d
wizzense <37890504+wizzense@users.noreply.github.com>
2025-03-07 07:11:24 -0800

    removed cleanup of project files

--- 0000_Cleanup-Files.ps1
+++ 0000_Cleanup-Files.ps1
@@ -3,7 +3,12 @@ Param(
     [PSCustomObject]$Config
 )
 
-# Define local path (fallback if not in config)
+<# Define local path (fallback if not in config)
+
+Doesn't work because this will be running from the root of the repo, not the user's home directory.
+
+Can update this later to copy the script to C:\temp and run it from there.
+
 $localPath = if ($config.LocalPath) { $config.LocalPath } else { "$env:USERPROFILE\Documents\ServerSetup" }
 
 # Ensure local directory exists
@@ -11,6 +16,8 @@ Write-Host "Ensuring local path '$localPath' exists..."
 if ((Test-Path $localPath)) {
     Remove-Item -Recurse -Force -Path $localPath
 }
+
+#>
 
 $InfraPath = if ($config.InfraRepoPath) { $config.InfraRepoPath } else { "C:\Temp\base-infra" }
 
